2ccb8e0402902f27c643a7530a1afc5b5f8d4223
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2019-11-30 13:47:20 +0500

    routing issue fixed

--- php/admin.php
+++ php/admin.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,20 +9,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap" rel="stylesheet">
     <link href="../css/vendors/fontawesome-free-5.11.2-web/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/main.css">
 </head>
 
 <body>
-    <div class="container">
+    <div class="main-container">
+        <?php
+        ini_set("display_errors", 1);
+        error_reporting(E_ALL);
+        include("MySql.php");
+        $user = unserialize($_SESSION["userObj"]);
+        $crud = new MySql();
+        if($user->getType()==1){
+            header("Location: user.php");
+        }
+        ?>
 
+   
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/additional-methods.min.js"></script>
-    <script src="../scripts/login.js"></script>
+   
 </body>
 
 </html>
--- php/user.php
+++ php/user.php
@@ -10,7 +10,6 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap" rel="stylesheet">
-    <link href="../css/vendors/fontawesome-free-5.11.2-web/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/main.css">
 </head>
 
@@ -22,6 +21,9 @@ session_start();
         include("MySql.php");
         $user = unserialize($_SESSION["userObj"]);
         $crud = new MySql();
+        if($user->getType()==0){
+            header("Location: admin.php");
+        }
         ?>
 
         <div class="table-container">
@@ -128,10 +130,7 @@ session_start();
 
         </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/additional-methods.min.js"></script>
-    <script src="../scripts/login.js"></script>
+
 </body>
 
 </html>
